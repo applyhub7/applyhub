353eefbc2b9852e17d0499875a9cc8c2ddbfe092
noseyug <vansonnguyen220905@gmail.com>
2026-07-15 18:29:01 +0700

    chore: update format check

--- backend/application-service/.eslintrc.js
+++ backend/application-service/.eslintrc.js
@@ -1,13 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   {
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off"
-    }
-  }
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
 ];
